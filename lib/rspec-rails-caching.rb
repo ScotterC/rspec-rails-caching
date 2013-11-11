b4e72b46d6a96f6c22452e1e2b5fe09ad553b58f
Scott Carleton <scott@artsicle.com>
2013-11-11 11:04:43 -0500

    rejigerring of code

--- lib/rspec-rails-caching.rb
+++ lib/rspec-rails-caching.rb
@@ -6,27 +6,32 @@ require_relative 'rspec-rails-caching/test_store'
 require_relative 'rspec-rails-caching/extensions/action_controller'
 
 module RSpecRailsCaching
+  # TODO: this code is in spec helper
+  # RSpec.configure do |config|
+  #   if ENV["CACHING"]
+  #     config.treat_symbols_as_metadata_keys_with_true_values = true
 
-  RSpec.configure do |config|
-    if ActionController::Base.perform_caching
-      silence_warnings do
-        Object.const_set :RAILS_CACHE, TestStore.new(do_read_cache: true)
-      end
-      ActionController::Base.cache_store = RAILS_CACHE
-      ActionController::Base.class_eval do
-        extend Extensions::ActionController::ClassMethods
-      end
+  #     silence_warnings do
+  #       Object.const_set :RAILS_CACHE, TestStore.new(do_read_cache: true)
+  #     end
 
-      config.include Matchers
+  #     ActionController::Base.cache_store = RAILS_CACHE
 
-      config.before :each do |example|
-        RAILS_CACHE.reset
-      end
+  #     ActionController::Base.class_eval do
+  #       extend Extensions::ActionController::ClassMethods
+  #     end
 
-      config.after :each do |example|
-        RAILS_CACHE.reset
-      end
-    end
-  end
+  #     include Matchers
+
+  #     config.around(:each, :caching) do |example|
+  #       RAILS_CACHE.reset
+  #       example.run
+  #       RAILS_CACHE.reset
+  #     end
+  #   else
+  #     config.filter_run_excluding :caching
+  #     ActionController::Base.perform_caching = false
+  #   end
+  # end
 
 end
